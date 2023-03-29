49bf65006e4d57cd78862486e45580845d122085
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-29 10:57:53 -0600

    add iteration3 methods

--- lib/team.rb
+++ lib/team.rb
@@ -15,4 +15,32 @@ class Team
     @roster.length
   end
 
+  def long_term_players
+    @roster.find_all do |player|
+      player.contract_length > 24
+    end
+  end
+
+  def short_term_players
+    @roster.find_all do |player|
+      player.contract_length <= 24
+    end
+  end
+
+  def total_value
+    total_value_array = @roster.map do |player|
+      player.contract_length * player.monthly_cost
+    end
+    total_value_array.sum
+  end
+
+
+  def details
+    {
+      "total_value" => total_value,
+      "player_count" => player_count
+    }
+
+  end
+
 end
